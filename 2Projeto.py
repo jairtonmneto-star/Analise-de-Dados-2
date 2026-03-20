26ce22a1c30af3ae45e17a40fbd5c188f4e67618
jairtonmneto-star <jairtonmneto@gmail.com>
2026-03-20 18:51:27 -0300

    Atualizacao do Projeto

--- 2Projeto.py
+++ 2Projeto.py
@@ -1,15 +1,25 @@
+import streamlit as st
 import pandas as pd
 import plotly.express as px
+#Titulo
+st.title("Dashboar de Vendas")
+#Carregar Dados
 Grafico=pd.read_csv("NetFlix.csv")
-display(Grafico.info())
+#Limpar Dados
 Grafico=Grafico.dropna()
-display(Grafico.info())
-#Separar Generos 
+#Filtro Interativos
+anos=st.multiselect("Escolha o Ano de Lancamento:",
+                    options=Grafico["release_year"].unique(),
+                    default=Grafico["release_year"].unique()
+)
+#Aplicar Filtro 
+Grafico=Grafico[Grafico["release_year"].isin(anos)]
+#Separar Generos
 df_generos=Grafico["genres"].str.split(", ")
-#Tranformar em Linhas
 df_generos=df_generos.explode()
-#Contar 
-top_generos = df_generos.value_counts().head(10)
+#Contar os Top 10
+top_generos=df_generos.value_counts().head(10)
 #Criar Grafico
-grafico= px.bar(x=top_generos.index,y=top_generos.values,text=top_generos.values,title="Top 10 Generos da Netflix")
-display(grafico)
+fig= px.bar(x=top_generos.index,y=top_generos.values,text=top_generos.values,title="Top 10 Generos da Netflix")
+#Mostrar grafico
+st.plotly_chart(fig)
